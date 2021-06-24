5d73f60ac4db0f13bb295a63af6dab237c45979f
Manan Doshi <manandoshi1301@gmail.com>
2021-06-25 01:34:46 +0530

    Resolved space

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Home from './Components/Home/Home';
 import styled from 'styled-components';
 
 function App() {
-  const Box = styled.div `
+  const Box = styled.div`
     font-family: 'Poppins', sans-serif;
   `
   return (
     <ThemeProvider theme={theme}>
       <Box>
-      <div className="App">
-        <Home />        
-      </div>
+        <div className="App">
+          <Home />
+        </div>
       </Box>
     </ThemeProvider>
 
