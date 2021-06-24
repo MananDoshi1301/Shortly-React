95d9b81b1b75525318e5735ba0374f8a56cd4558
Manan Doshi <manandoshi1301@gmail.com>
2021-06-25 01:34:22 +0530

    Added nav and banner

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Nav from './Nav/Nav';
+import styled from 'styled-components';
+import {space} from 'styled-system'
+import Banner from './Banner/Banner';
 
 const Home = () => {
+
+    const Box = styled.div `${space}`
+
     return (
         <div>
-            <Nav />
+            <Box mx={[3, 5, 6]} my={4}>
+                <Nav />
+            </Box>
+            <Box ml={[3, 5, 6]} my={4}>
+                <Banner /> 
+            </Box>
         </div>
     )
 }
