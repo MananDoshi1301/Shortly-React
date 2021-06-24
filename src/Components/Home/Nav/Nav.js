808212a8d9623f7fdd11cb1218b137386468ca92
Manan Doshi <manandoshi1301@gmail.com>
2021-06-25 01:33:43 +0530

    Nav configured

--- src/Components/Home/Nav/Nav.js
+++ src/Components/Home/Nav/Nav.js
@@ -1,62 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color, space } from 'styled-system';
+import { color } from 'styled-system';
 import logo from '../../../Assets/Images/logo.svg';
 
 const Nav = () => {
 
-    const Box = styled.div `${space}`
-    
     const Nav = styled.nav`
-        ${color}
+        
     `
-    const FlexBetween = styled.div `
+    const FlexBetween = styled.div`
         display: flex;
         justify-content: space-between;
         align-items: center;
+        gap: 30px;
     `
 
-    const Link = styled.a `
-        ${color}
+    const Link = styled.a`
+        ${color};
+        font-weight: 700;
         text-decoration: none;
-        font-size: 20px;
+        font-size: 15px;
+    `
 
-        gap: 5;
+    const PageLink = styled(Link) `
+        &:hover{
+            color:'#35323E';
+        }
     `
 
     const pageLinks = [
-        {name:'Features'},
-        {name:'Pricing'},
-        {name:'Resources'},
+        { name: 'Features' },
+        { name: 'Pricing' },
+        { name: 'Resources' },
     ]
 
     const userLinks = [
-        {name:'Login'},
-        {name:'Sign Up'},
+        { name: 'Login' },
+        { name: 'Sign Up' },
     ]
     return (
         <div>
-            <Box mx={6} my={4}>
-                <Nav>
-                    <FlexBetween>
-                        <div>
-                            <FlexBetween>
-                                <img src={logo} alt="" />
-                                <FlexBetween>
-                                    {pageLinks.map((link, key)=>(
-                                        <Link color={'themeGray'} href='#' key={key}>{link.name}</Link>
-                                    ))}
-                                </FlexBetween>
-                            </FlexBetween>
-                        </div>
+
+            <Nav>
+                <FlexBetween>
+                    <div>
                         <FlexBetween>
-                            {userLinks.map((link, key)=>(
-                                <Link color={'themeGray'}  href='#' key={key}>{link.name}</Link>
-                            ))}
+                            <img src={logo} alt="" />
+                            <FlexBetween>
+                                {pageLinks.map((link, key) => (
+                                    <PageLink color={'themeDarkGray'} href='#' key={key}>{link.name}</PageLink>
+                                ))}
+                            </FlexBetween>
                         </FlexBetween>
+                    </div>
+                    <FlexBetween>
+                        {userLinks.map((link, key) => (
+                            <Link color={'themeDarkGray'} href='#' key={key}>{link.name}</Link>
+                        ))}
                     </FlexBetween>
-                </Nav>
-            </Box>
+                </FlexBetween>
+            </Nav>
         </div>
     )
 }
