80ad124093d0cde5cc4c32ab96dd2579e161b0ca
Manan Doshi <manandoshi1301@gmail.com>
2021-06-27 15:56:31 +0530

    Base home file configured

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,18 +3,42 @@ import Nav from './Nav/Nav';
 import styled from 'styled-components';
 import {space} from 'styled-system'
 import Banner from './Banner/Banner';
+import CardContent from './CardContent/CardContent'
+import CTAContent from './CTAContent/CTAContent';
+import Footer from './Footer/Footer';
 
 const Home = () => {
 
     const Box = styled.div `${space}`
 
+    const BannerWrapper = styled.div `
+        @media (max-width: 48em){
+            margin: 32px 16px;
+        }
+
+        @media (min-width: 48em){
+            margin: 128px;
+        }
+    `
+
     return (
         <div>
-            <Box mx={[3, 5, 6]} my={4}>
+            <Box mx={[3, 5, 6]} mt={4} mb={5}>
                 <Nav />
             </Box>
-            <Box ml={[3, 5, 6]} my={4}>
-                <Banner /> 
+            <Box my={4}>
+                <BannerWrapper>
+                    <Banner /> 
+                </BannerWrapper>
+            </Box>
+            <Box>
+                <CardContent />
+            </Box>
+            <Box>
+                <CTAContent />
+            </Box>
+            <Box>
+                <Footer />
             </Box>
         </div>
     )
